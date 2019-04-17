dd59e3bedf27ba10edf7119db9363119e723de36
uraniumpotato96 <36888694+uraniumpotato96@users.noreply.github.com>
2019-04-17 12:57:22 +0530

    Update guessingGameAssignment.py

--- guessingGameAssignment.py
+++ guessingGameAssignment.py
@@ -1,3 +1,16 @@
+""""
+In this game we will add a counter for how many guesses the user can have.
+
+The counter is initial set to zero.
+
+The while loop will run as long as the guesses are less to 5.
+
+If the user guess the right number before that, the script will break and
+present the user with how many guesses it took to guess the right number.
+
+The variables in this script can be changed to anything.
+"""
+
 from random import randint
 
 z = randint(1,10)
