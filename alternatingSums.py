b8fae5fcedf7ff66aab60e863af684fae1cbed96
uraniumpotato96 <36888694+uraniumpotato96@users.noreply.github.com>
2019-04-29 09:57:00 +0530

    Add files via upload

---
+++ alternatingSums.py
@@ -0,0 +1,26 @@
+"""
+Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+
+You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+
+Example
+
+For a = [50, 60, 60, 45, 70], the output should be
+alternatingSums(a) = [180, 105].
+"""
+
+def alternatingSums(a):
+    team1=[]
+    team2=[]
+    flag = True
+    for i in a:
+        if flag:
+            team1.append(i)
+            flag = False
+        else:
+            team2.append(i)
+            flag = True
+
+    return [sum(team1),sum(team2)]
+
+print(alternatingSums([50, 60, 60, 45, 70]))
