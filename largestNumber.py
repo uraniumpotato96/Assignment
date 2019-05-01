afbe3bb3fe03cf1f49a934d9c330edb74b20fa0c
uraniumpotato96 <36888694+uraniumpotato96@users.noreply.github.com>
2019-05-01 12:39:07 +0530

    Add files via upload

---
+++ largestNumber.py
@@ -0,0 +1,12 @@
+"""
+Given an integer n, return the largest number that contains exactly n digits.
+
+Example
+
+For n = 2, the output should be
+largestNumber(n) = 99.
+
+"""
+
+def largestNumber(n):
+    return int('9'*n)
